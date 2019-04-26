fbf5c364ed091836ea8c865022acebf9064f2977
DrMad92 <mmadraimov@gmail.com>
2019-04-27 02:37:28 +0300

    Change Dockerfile. Removed unused endpoints. New data.

--- backend/main.go
+++ backend/main.go
@@ -17,37 +17,35 @@ var (
 )
 
 type query struct {
-	Country   string `json:"country"`
-	Product   string `json:"product"`
-	Revenue   string `json:"revenue"`
-	Threshold string `json:"threshold"`
+	Country string `json:"country"`
 }
 
 type countryData struct {
-	StandardRate string            `json:"standardRate"`
-	Categories   map[string]string `json:"categories"`
+	StandardRate string         `json:"standardRate"`
+	Threshold    string         `json:"threshold"`
+	Categories   []categoryData `json:"categories"`
 }
 
+type categoryData struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Comments    string `json:"comments"`
+	ReducedRate string `json:"reducedRate"`
+}
 type Query interface {
 	fetchData()
 }
 
 var eeJsonServices = []string{
 	"http://ec.europa.eu/taxation_customs/tedb/api/search/98c6a5ee-04e0-4608-9a26-24d1a91f5ae8.json",
-	"http://ec.europa.eu/taxation_customs/tedb/api/search/f6b62d72-a14b-4cec-b58b-1ae50ae290c9.json",
-	"http://ec.europa.eu/taxation_customs/tedb/api/search/4a02e91b-9f9b-4f7b-a21b-8af3fb5558ba.json",
 }
 
 var fiJsonServices = []string{
 	"http://ec.europa.eu/taxation_customs/tedb/api/search/a19cb68c-0d12-48f7-87f2-7cba255385ff.json",
-	"http://ec.europa.eu/taxation_customs/tedb/api/search/6b344392-cba4-4a34-a391-ebbb66fbf852.json",
-	"http://ec.europa.eu/taxation_customs/tedb/api/search/fe151deb-9e03-4739-af3b-afc06dd7219e.json",
 }
 
 var deJsonServices = []string{
 	"http://ec.europa.eu/taxation_customs/tedb/api/search/8bf428cb-9152-46a3-b66a-3a922a4115cb.json",
-	"http://ec.europa.eu/taxation_customs/tedb/api/search/85e5e148-1e7a-47e9-b90e-022ede929c84.json",
-	"http://ec.europa.eu/taxation_customs/tedb/api/search/7d5969a0-0fcc-4eb8-a0e8-cb919f578099.json",
 }
 
 func main() {
@@ -78,9 +76,10 @@ func main() {
 }
 
 func (q query) fetchData() countryData {
-	var data countryData
-	data.Categories = make(map[string]string)
-	url := make([]string, 3)
+	var country countryData
+	country.Categories = make([]categoryData, 50)
+
+	url := make([]string, 1)
 
 	switch q.Country {
 	case "EE":
@@ -90,7 +89,7 @@ func (q query) fetchData() countryData {
 	case "DE":
 		copy(url, deJsonServices)
 	default:
-		return data
+		return country
 	}
 
 	for _, u := range url {
@@ -104,18 +103,20 @@ func (q query) fetchData() countryData {
 				log.Fatal(err)
 			}
 			bodyString := string(bodyBytes)
-			if gjson.Get(bodyString, `results.0.standardRate`).Exists() {
-				data.StandardRate = gjson.Get(bodyString, `results.0.standardRate`).String()
-			}
+			country.StandardRate = gjson.Get(bodyString, `results.0.standardRate`).String()
+			country.Threshold = gjson.Get(bodyString, `results.0.specialSchemeDistanceSellingThreshold`).String()
 			categoryList := gjson.Get(bodyString, "results.0.reducedRates")
 			for _, name := range categoryList.Array() {
-				categoryName := gjson.Get(name.String(), "category.name").String()
-				reducedRate := gjson.Get(name.String(), "reducedRate").String()
-				data.Categories[categoryName] = reducedRate
+				var category categoryData
+				category.Name = gjson.Get(name.String(), "category.name").String()
+				category.ReducedRate = gjson.Get(name.String(), "reducedRate").String()
+				category.Comments = gjson.Get(name.String(), "comments").String()
+				category.Description = gjson.Get(name.String(), "category.description").String()
+				country.Categories = append(country.Categories, category)
 			}
 		}
 		res.Body.Close()
 	}
 
-	return data
+	return country
 }
